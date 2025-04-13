5086f94bab01a764f5af819e7344f41cb1423099
Axel Fernández <axelfeji@gmail.com>
2025-04-13 16:34:47 -0600

    Spotify API Connected & Get Track Metadata

---
+++ spotify-api/spotify-api/src/test/java/com/example/spotify_api/SpotifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotify_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spotify-api/src/main/java/com/example/spotify_api/SpotifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotify_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 	SpotifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyApiApplication.class, args);
+	}
+
+}
